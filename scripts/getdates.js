b7066865d8a98ee0059bcfd90330a9a31bd49e7e
savannamw2 <whi19068@byui.edu>
2024-01-08 17:01:38 -0800

    Week1

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+var currentYear = new Date().getFullYear();
+
+document.getElementById("currentYear").innerHTML = currentYear;
+
+var lastModified = document.lastModified;
+
+document.getElementById("lastModified").innerHTML = 'Last Modified: '+lastModified;
