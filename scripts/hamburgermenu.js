c074ca85b881c8d4201e8773804448612ad3146a
savannamw2 <whi19068@byui.edu>
2024-02-20 13:47:47 -0800

    lazy load

--- scripts/hamburgermenu.js
+++ scripts/hamburgermenu.js
@@ -2,6 +2,6 @@ const hambutton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
 hambutton.addEventListener('click', () => {
-    navigation.classList.toggle('show');
-    hambutton.classList.toggle('show');
+    navigation.classList.toggle('open');
+    hambutton.classList.toggle('open');
 });
